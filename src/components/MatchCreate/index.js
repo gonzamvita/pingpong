73fd48ac2415c254a0e820ea23f72adc99d40d07
jbelmaro <jbelmaro@gmail.com>
2020-04-04 20:55:27 +0200

    New Match Form created

--- src/components/MatchCreate/index.js
+++ src/components/MatchCreate/index.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import 'date-fns';
 import { Link } from 'react-router-dom';
-
-import { FormHelperText, TextField, Button } from '@material-ui/core';
+import {TextField, Button, Typography, 
+    RadioGroup, Radio, FormControlLabel, FormLabel, FormControl} from '@material-ui/core';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardDatePicker,
+} from '@material-ui/pickers';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 
 import { withAuthUser } from '../Session';
 import * as ROUTES from '../../constants/routes';
+import { withTranslation } from 'react-i18next';
 
 class MatchCreatePage extends Component {
     constructor(props) {
@@ -18,7 +25,10 @@ class MatchCreatePage extends Component {
                 host_uid: '',
                 opponent: '',
                 opponent_uid: '',
-                status: 'pending'
+                status: 'pending',
+                match_date: '',
+                players: '1vs1',
+                match_type: 'friendly'
             },
         };
         console.log(this.props)
@@ -53,26 +63,76 @@ class MatchCreatePage extends Component {
     }
 
     onChange = (e, value) => {
+        const match = this.state.match
         this.setState({
             match: {
                 host: this.state.currentUser.username,
                 host_uid: this.state.currentUser.uid,
                 opponent: value.username,
-                opponent_uid: value.uid,
+                match_date: match.match_date,
+                players: match.players,
+                match_type: match.match_type
+            }
+        }
+        );
+    };
+
+    onChangeMatchDate = (e, value) => {
+        const match = this.state.match
+        this.setState({
+            match: {                
+                host: match.host,
+                host_uid: match.host_uid,
+                opponent: match.opponent,
+                match_date: value,
+                players: match.players,
+                match_type: match.match_type            
+            }
+        }
+        );
+    };
+    onChangePlayers = (e, value) => {
+        const match = this.state.match
+        this.setState({
+            match: {                
+                host: match.host,
+                host_uid: match.host_uid,
+                opponent: match.opponent,
+                match_date: match.match_date,
+                players: value,
+                match_type: match.match_type            
+            }
+        }
+        );
+    };
+    onChangeMatchType = (e, value) => {
+        const match = this.state.match
+        this.setState({
+            match: {                
+                host: match.host,
+                host_uid: match.host_uid,
+                opponent: match.opponent,
+                match_date: match.match_date,
+                players: match.players,
+                match_type: value            
             }
         }
         );
     };
 
     render() {
-        const { users, loading } = this.state;
+        const { users, loading} = this.state;
+        const { t } = this.props;
+        let match_date, match_type, players;
 
         console.log(this.props)
         return (
             <div>
-                <h1>New Match</h1>
-                <form>
-                    <FormHelperText>Challenge a player to play a match</FormHelperText>
+                <FormControl>
+                    <Typography variant="h5" gutterBottom>
+                    {t('new_match')}
+                    </Typography>
+                    <FormLabel component="legend">{t('create_match')}</FormLabel>
                     <Autocomplete
                         id="opponent"
                         loading={loading}
@@ -84,8 +144,28 @@ class MatchCreatePage extends Component {
                         getOptionLabel={(user) => user.username}
                         renderInput={(params) => <TextField {...params} label="Opponent" variant="outlined" />}
                     />
-                    <Button type="submit" variant="contained" onClick={e => this.onSubmit(e)}>Create</Button>
-                </form>
+                    <FormLabel component="legend">{t('match_type')}</FormLabel>
+                    <RadioGroup defaultValue="friendly" aria-label="match_type" name="match_type1" value={match_type} onChange={this.onChangeMatchType}>
+                        <FormControlLabel value="ranked" control={<Radio />} label={t('ranked')} />
+                        <FormControlLabel value="friendly" control={<Radio />} label={t('friendly')} />
+                    </RadioGroup>
+                    <FormLabel component="legend">{t('players')}</FormLabel>
+                    <RadioGroup defaultValue="1vs1" aria-label="players" name="players1" value={players} onChange={this.onChangePlayers}>
+                        <FormControlLabel value="1vs1" control={<Radio />} label="1vs1" />
+                        <FormControlLabel value="2vs2" control={<Radio />} label="2vs2" />
+                    </RadioGroup>
+                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                        <KeyboardDatePicker
+                            margin="normal"
+                            id="date-picker-dialog"
+                            label={t('match_date')}
+                            format="dd/MM/yyyy"
+                            value={match_date}
+                            onChange={this.onChangeMatchDate}
+                        />
+                    </MuiPickersUtilsProvider>
+                    <Button type="submit" variant="contained" onClick={e => this.onSubmit(e)}>{t('create')}</Button>
+                </FormControl>
             </div>
         );
     }
@@ -97,5 +177,5 @@ const NewMatchLink = () => (
     </p>
 );
 
-export default withAuthUser(MatchCreatePage);
+export default withAuthUser(withTranslation()(MatchCreatePage));
 export { NewMatchLink };
--- src/components/Session/withAuthUser.js
+++ src/components/Session/withAuthUser.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 import AuthUserContext from './context';
 import { withFirebase } from '../Firebase';
+import { withTranslation } from 'react-i18next';
 
 const withAuthUser = Component => {
     function WithAuthUser(props) { //componente, mayus recibe props devuelve algo que se renderiza
         const authUser = React.useContext(AuthUserContext)
         return (
-            <Component {...props} authUser={authUser} />
+            <Component {...props} authUser={authUser} withTranslation={withTranslation}/>
         );
     }
     return withFirebase(WithAuthUser);
